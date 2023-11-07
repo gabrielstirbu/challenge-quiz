e32b8764d41d90c4d8ed6172a9c9867d6a50f25d
GabrielCS22 <108716323+GabrielCS22@users.noreply.github.com>
2023-11-07 17:51:38 +0000

    Adding the final fuctions and fixing the start button as I forgot to add event on the start button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,14 +4,19 @@ var startButton = document.getElementById("start");
 var questionTitle = document.getElementById("question-title");
 var choicesDiv = document.getElementById("choices");
 var timeSpan = document.getElementById("time");
+var feedbackDiv = document.getElementById("feedback");
+var initialsInput = document.getElementById("initials");
+var submitButton = document.getElementById("submit");
+var finalScoreSpan = document.getElementById("final-score");
 
 //Empty var (starting point)
 var currentQuestionIndex = 0;
-var timeLeft = 15;
+var timeLeft = 3;
 var score = 0;
 var timerInterval;
 
 // WHEN I click the start button
+startButton.addEventListener("click", startQuiz);
 // Function to start the quiz
 function startQuiz() {
     // Hide the start screen and show the questions screen
@@ -20,41 +25,66 @@ function startQuiz() {
     // Start the timer
     startTimer();
     // Load the first question
-    loadQuestion(currentQuestion[i]);
+    loadQuestion(currentQuestionIndex);
 }
 
 // Function to load a question
-function loadQuestion(i) {
-    questionTitle.textContent = questions[i].question;
+function loadQuestion(index) {
+    questionTitle.textContent = questions[index].question;
     choicesDiv.innerHTML = "";
 
-    for (let i = 0; i < questions[i].answers.length; i++) {
+    for (let i = 0; i < questions[index].answers.length; i++) {
         const choiceButton = document.createElement("button");
-        choiceButton.textContent = questions[i].answers[i];
+        choiceButton.textContent = questions[index].answers[i];
         choiceButton.addEventListener("click", handleAnswerClick);
         choicesDiv.appendChild(choiceButton);
     }
 }
-
+// THEN a timer starts 
 // Function to start the timer
 function startTimer() {
     timerInterval = setInterval(function () {
-      timeLeft--;
-      timeSpan.textContent = timeLeft;
-  
-      if (timeLeft <= 0) {
-        clearInterval(timerInterval);
-        endQuiz();
-      }
-    }, 1000);
-  }
+        timeLeft--;
+        timeSpan.textContent = timeLeft;
 
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+// Function to handle user's answer click
+function handleAnswerClick(event) {
+    var selectedAnswer = event.target.textContent;
+    var correctAnswer = questions[currentQuestionIndex].answers[questions[currentQuestionIndex].correctAnswerIndex];
+
+    if (selectedAnswer === correctAnswer) {
+        // Correct answer
+        score += 1;
+        feedbackDiv.textContent = "Correct!";
+    } else {
+        // Incorrect answer
+        timeLeft -= 1;
+        feedbackDiv.textContent = "Incorrect!";
+    }
+
+    feedbackDiv.classList.remove("hide");
+
+    // Move to the next question or end the quiz
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        loadQuestion(currentQuestionIndex);
+    } else {
+        endQuiz();
+    }
+}
+
+// Function to end the quiz
+function endQuiz() {
+    clearInterval(timerInterval);
+    timeSpan.textContent = timeLeft;
+    finalScoreSpan.textContent = score;
+    document.getElementById("questions").classList.add("hide");
+    document.getElementById("end-screen").classList.remove("hide");
+}
