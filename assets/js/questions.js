164693fe84478dbb20bba672784a15ef28d37bdf
GabrielCS22 <108716323+GabrielCS22@users.noreply.github.com>
2023-11-07 00:21:26 +0000

    Adding questions and aswears for the game

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+var questions = [
+    {
+        question: "What does the acronym 'DOM' stand for in JavaScript?",
+        answers: ["Document Object Model", "Data Object Model", "Dynamic Object Manipulation", "Digital Object Management"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What is the purpose of 'console.log()' in JavaScript?",
+        answers: ["Display a message to the user", "Perform mathematical calculations", "Print output to the browser console", "Create a pop-up dialog"],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        answers: ["variable", "var", "declare", "let"],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is the result of the expression '2 + '2' in JavaScript?",
+        answers: ["4", "22", "NaN", "Error"],
+        correctAnswerIndex: 1
+    }
+];
+
